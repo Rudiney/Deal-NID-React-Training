4725da4668b066cb138bc45b4dae5b979560766c
Rudiney Franceschi <rudi.atp@gmail.com>
2021-11-21 21:24:56 -0300

    basic app menu

---
+++ reference/04_0_app_menu/src/App.js
@@ -0,0 +1,54 @@
+import cn from "classnames";
+
+const menuItems = [
+  { name: "Dashboard", href: "#", current: true },
+  { name: "Team", href: "#", current: false },
+  { name: "Projects", href: "#", current: false },
+  { name: "Calendar", href: "#", current: false },
+  { name: "Reports", href: "#", current: false },
+];
+
+export default function Example() {
+  return (
+    <>
+      <div className="min-h-full">
+        <nav className="bg-gray-800">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="h-16 flex items-center space-x-4">
+              {menuItems.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={cn(
+                    item.current
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "px-3 py-2 rounded-md text-sm font-medium"
+                  )}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </nav>
+
+        <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+
+        <main>
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            {/* Replace with your content */}
+            <div className="px-4 py-6 sm:px-0">
+              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
+            </div>
+            {/* /End replace */}
+          </div>
+        </main>
+      </div>
+    </>
+  );
+}
---
+++ reference/04_0_app_menu/src/utils/calculateWinner.js
@@ -0,0 +1,20 @@
+const WinnerLines = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+export default function calculateWinner(squares) {
+  for (let i = 0; i < WinnerLines.length; i++) {
+    const [a, b, c] = WinnerLines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
